b821623467562144b2b292f27e820d5426de429b
John R Iversen <john@johniversen.org>
2014-06-04 21:55:38 -0700

    corr.R: correct and complete

--- corr.R
+++ corr.R
@@ -8,4 +8,16 @@ corr <- function(directory, threshold = 0) {
   ## nitrate and sulfate; the default is 0
   
   ## Return a numeric vector of correlations
+  
+  corr <- vector(mode="double") #NB: must be numeric, per instructions
+  ncomplete <- complete(directory)
+  goodidx <- ncomplete[["nobs"]] > threshold #NB must be > not >= because default=0
+  id <- ncomplete[["id"]][goodidx]
+  for (i in seq_along(id)) {
+    fname <- paste(directory, "/", sprintf("%03d",id[i]),'.csv',sep="")
+    sensordata <- read.csv(fname)
+    gooddata <- sensordata[complete.cases(sensordata),]
+    corr[i] = cor(gooddata[["sulfate"]], gooddata[["nitrate"]])
+  }
+  corr
 }
